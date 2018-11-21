62c9b2a410c95c9c22a051ab0893e7b1803cefc5
Dmytro Sumtsov <sumtsow@gmail.com>
2018-11-21 23:12:16 +0200

    Update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,12 +2,11 @@
 @section('scripts')
     <script src="{{ asset('js/swfobject.js') }}" type="text/javascript" language="javascript"></script>
     <script src="{{ asset('js/swf.js') }}" type="text/javascript" language="javascript"></script>
-@endsection    
+@endsection
+@section('nav')
+    @include('nav') 
+@endsection
 @section('content')
-<h2>Content</h2>
-<h4>Header</h4>
-<img src = "auto1.jpg" class = "img-responsive">
-<p>Lorem ipsum doloret yuio ffghgf fdfgfhf ttyuytg gjgfj gfgjgf ghgfjgf gfgugf ghjkghkhg gfjgj utytiytik fdhbfbvcv fdhfdhfh  gngnggf fdhfdhfdhfd fdhfdhfdhfdhffd  hfdhfdhfdhfd hdfhfdhfdhf hfdhfd fdhfdhfdhdf fdhfdhfdhhfd hfdhd</p>
 <!-- <div id="myglobe">
 <h1>Alternative content</h1>
 <p><a href="http://www.adobe.com/go/getflashplayer"><img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Get Adobe Flash player" /></a></p>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,44 +10,35 @@
     <meta charset="utf-8" />
     <link rel="shortcut icon" href="favicon.ico">
     <!-- Scripts -->
-    <!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script> -->
-@yield('scripts')
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    @yield('scripts')
     <!-- Styles -->
-    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"> -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
 </head>
 <body>
-<div class="wrapper container">
+<div class="container-fluid">
     <header>{LANGFORM} {BANNER}</header>
-    <nav class="navbar navbar-default">
-	<ul class="nav navbar-nav">
-		<li class="active"><a href="/home/">Home</a></li>
-		<li><a href="/about/">About us</a></li>
-		<li><a href="/services/">Services</a></li>
-		<li><a href="/partners/">Partners</a></li>
-		<li><a href="/customers/">Customers</a></li>
-		<li><a href="/projects/">Projects</a></li>
-		<li><a href="/careers/">Careers</a></li>
-		<li><a href="/contact/">Contact</a></li>			
-	</ul>
+    <nav class="nav nav-pills justify-content-left">
+	<a class="nav-link active" href="/home/">Home</a>
+	<a class="nav-link" href="/about/">About us</a>
+	<a class="nav-link" href="/services/">Services</a>
+	<a class="nav-link" href="/partners/">Partners</a>
+	<a class="nav-link" href="/customers/">Customers</a>
+	<a class="nav-link" href="/projects/">Projects</a>
+	<a class="nav-link" href="/careers/">Careers</a>
+	<a class="nav-link" href="/contact/">Contact</a>			
     </nav>
     <div class="heading">
 	<h1>Pat Phy Dep Site</h1>
     </div>
     <div class="row">
         <aside class="col-md-3">
-        <ul class="list-group submenu">
-                <li class="list-group-item active"><a href="/">Home</a></li>
-		<li class="list-group-item"><a href="/about/">About us</a></li>
-		<li class="list-group-item"><a href="/services/">Services</a></li>
-		<li class="list-group-item"><a href="/partners/">Partners</a></li>
-		<li class="list-group-item"><a href="/customers/">Customers</a></li>
-		<li class="list-group-item"><a href="/projects/">Projects</a></li>
-		<li class="list-group-item"><a href="/careers/">Careers</a></li>
-		<li class="list-group-item"><a href="/contact/">Contact</a></li>
-        </ul>
+            @yield('nav')
         </aside>
-        <section class="col-md-7">
+        <section class="col-md-9">
             @yield('content')
         </section>
     </div>

361473dd348b1449646dce5bb8179553b3587e44
Dmytro Sumtsov <sumtsow@gmail.com>
2018-11-26 22:42:41 +0200

    Update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <title>{{ config('app.title.ru')}}</title>
@@ -16,56 +16,48 @@
     @yield('scripts')
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/pat.css') }}" rel="stylesheet">    
 </head>
 <body>
 <div class="container-fluid">
-    <div class="row">    
-        <div class="card border-dark w-100 m-3">
-            <div class="card-body p-0">
-                <img src="/img/title_ru.jpg" class="img-fluid w-100" alt="Pat Phy Dep Site">
-                <header>{LANGFORM}</header>
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="card border-dark w-100 mr-3 ml-3">
-                <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img class="d-block w-100" src="/img/img01.jpg" alt="Перший слайд">
-                        </div>
-                        <div class="carousel-item">
-                            <img class="d-block w-100" src="/img/img02.jpg" alt="Другий слайд">
-                        </div>
-                        <div class="carousel-item">
-                            <img class="d-block w-100" src="/img/img03.jpg" alt="Третій слайд">
-                        </div>
-                        <div class="carousel-item">
-                            <img class="d-block w-100" src="/img/img04.jpg" alt="Четвертий слайд">
-                        </div>
-                        <div class="carousel-item">
-                            <img class="d-block w-100" src="/img/img05.jpg" alt="П'ятий слайд">
-                        </div>
-                        <div class="carousel-item">
-                            <img class="d-block w-100" src="/img/img06.jpg" alt="Шостий слайд">
-                        </div>
-                        <div class="carousel-item">
-                            <img class="d-block w-100" src="/img/img07.jpg" alt="Сьомий слайд">
-                        </div>
-                    </div>
+    <div class="row mx-1 my-3" id="logo">@yield('langform')</div>
+    <div class="row mx-1">
+        <div id="carouselExampleSlidesOnly" class="carousel w-100 slide border-dark" data-ride="carousel">
+            <div class="carousel-inner" id="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="/img/carousel/img01.jpg" alt="Перший слайд">
                 </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="/img/carousel/img02.jpg" alt="Другий слайд">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="/img/carousel/img03.jpg" alt="Третій слайд">
+                </div>
+                <div class="carousel-item">
+                <img class="d-block w-100" src="/img/carousel/img04.jpg" alt="Четвертий слайд">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="/img/carousel/img05.jpg" alt="П'ятий слайд">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="/img/carousel/img06.jpg" alt="Шостий слайд">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="/img/carousel/img07.jpg" alt="Сьомий слайд">
+                </div>
+             </div>
         </div>
     </div>
-    <div class="row p-3">    
-        <div class="card col-sm-3 bg-success">
-            <nav class="nav">
+    <div class="row p-3  border-1">    
+        <div class="card col-sm-3 border-0 p-0">
+            <nav class="nav" id="nav">
                 @yield('nav')
             </nav>
         </div>
-        <div class="card col-sm-9 border-0 pl-3 pr-0">
-            <div class="card-header bg-success">
-                <nav class="nav" area-label="breadcrumb">
-                    <ol class="breadcrumb bg-success">
+        <div class="card col-sm-9 border-0 pl-3 pr-0 bg-transparent">
+            <div class="card-header m-0 p-0 border-0" id="breadcrumbs">
+                <nav class="nav my-0 py-0" area-label="breadcrumb">
+                    <ol class="breadcrumb m-0">
                         <li class="breadcrumb-item"><a href="/">Home</a></li>
                         @yield('breadcrumb')
                     </ol>
@@ -76,12 +68,12 @@
             </div>
         </div>
     </div>
-    <footer class="footer">
-        <div class="row justify-content-center">        
-            <div class="card w-100 text-white bg-success mr-3 mb-3 ml-3 rounded-bottom badge-pill">
-                <div class="card-body">
-                    <div class="row justify-content-center">   
-                        {COUNTERS} &copy; {{ config('app.title.ru')}}, 2015-{{date('Y')}}
+    <footer class="footer m-0 px-3">
+        <div class="row justify-content-center m-0 p-0">        
+            <div class="card w-100 mb-0 border-0 m-0 p-0">
+                <div class="card-body m-0 p-0">
+                    <div class="row justify-content-center my-0" id="foo">
+                        @yield('counters') &copy; {{ config('app.title.ru')}}, 2015-{{date('Y')}}
                     </div>
                 </div>
             </div>

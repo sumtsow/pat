4c1ec6ff781bf274ad3a34896d4caf05efbf5782
Dmytro Sumtsov <sumtsow@gmail.com>
2019-02-26 00:42:06 +0200

    Update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,7 +17,7 @@ class UserController extends Controller
     public function index()
     {
         return view('user.index', [
-            'users' => User::orderBy('name', 'asc')->paginate(\App::environment('paginate')),
+            'users' => User::getAll(),
         ]);
     }
 
